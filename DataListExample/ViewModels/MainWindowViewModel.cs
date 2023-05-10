f2b126017156743d5aa41454495aa8b826b3fb13
Strix <strix@fedora-37.fedora>
2023-05-10 16:42:27 +0800

    addendum

---
+++ DataListExample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DataListExample.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public ObservableCollection<string> Numbers { get; }
+    public ICommand AddRandomNumberToList { get; }
+    public ICommand DeleteRandomNumberToList { get; }
+
+    public MainWindowViewModel()
+    {
+        Numbers = new ObservableCollection<string>();
+        AddRandomNumberToList = new RelayCommand(OnAddRandomNumberToList);
+        DeleteRandomNumberToList = new RelayCommand(OnDeleteRandomNumberToList, () => Numbers.Count > 0);
+
+        for (var i = 0; i < 10; i++)
+        {
+            Numbers.Add($"{Random.Shared.Next(0, 100)}");
+        }
+    }
+
+    private void OnAddRandomNumberToList()
+    {
+        Numbers.Add($"{Random.Shared.Next(0, 100)}");
+    }
+
+    private void OnDeleteRandomNumberToList()
+    {
+        Numbers.RemoveAt(Random.Shared.Next(0, Numbers.Count - 1));
+    }
+}
---
+++ DataListExample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DataListExample.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
